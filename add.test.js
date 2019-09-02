8015113e27d20b0e695fc5cfa62e9d6148a64caf
nikhilknick <nikhilknick@gmail.com>
2019-09-02 17:45:44 +0530

    fixing multiply issues

--- add.test.js
+++ add.test.js
@@ -1,9 +1,7 @@
-import {add,mul} from './add';
+var add = require('./add');
+
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test('multiply 2 * 2 to equal 4', () => {
-  expect(mul(2, 2)).toBe(4);
-});
---
+++ mul.test.js
@@ -0,0 +1,6 @@
+var mul = require('./mul');
+
+
+test('multiply 2 * 2 to equal 4', () => {
+    expect(mul(2, 2)).toBe(4);
+  });
